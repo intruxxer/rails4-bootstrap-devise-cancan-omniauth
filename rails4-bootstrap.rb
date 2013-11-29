4d189b02d09472bb01c4743585e95482e4c23b8e
Ali Fahmi <lab@advanced-network.org>
2013-11-30 02:01:53 +0900

    Update rails4-bootstrap.rb

--- rails4-bootstrap.rb
+++ rails4-bootstrap.rb
@@ -61,7 +61,7 @@ end
 
 gem "bootstrap-sass", "~> 2.3.2.1"
 gem "font-awesome-sass-rails"
-gem "simple_form", github: "plataformatec/simple_form"
+gem "simple_form"
 gem "devise", "~> 3.0.0"
 gem "cancan"
 gem "omniauth"
