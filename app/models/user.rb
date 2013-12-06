fddc36ac98656aa7d8a2a32d7b88fb7dfd34ac87
Ali Fahmi <lab@advanced-network.org>
2013-12-06 15:55:11 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User
   field :image, type: String
   field :first_name, type: String
   field :last_name, type: String
+  field :passport, type: String
+  field :id_card, type: String
+  field :citizenship, type: Boolean, default: true
+
   field :roles_mask, type: Integer
   
   validates_presence_of :email, :first_name, :last_name
